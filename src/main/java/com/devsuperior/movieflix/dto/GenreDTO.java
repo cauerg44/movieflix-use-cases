2d8fda48f59e2547a90376dfe1f4991cade9b90d
Caue Garcia <cauerg7@outlook.com>
2024-02-23 16:00:16 -0300

    GET /genres validated

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,9 +1,24 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Genre;
+
 public class GenreDTO {
 
     private Long id;
     private String name;
+
+    public GenreDTO() {
+    }
+
+    public GenreDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public GenreDTO(Genre obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
 
     public Long getId() {
         return id;
